593deadb680b0b21b8a39180a50f228c767fa5cc
Celso Silvestre <clsoft.silvestre@gmail.com>
2021-03-31 19:28:12 +0000

    bugfix the dismiss of the popover

--- src/app/components/popover/popover.component.ts
+++ src/app/components/popover/popover.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-popover',
@@ -9,13 +10,14 @@ import { Router } from '@angular/router';
 })
 export class PopoverComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, public popoverController: PopoverController) { }
 
   ngOnInit() {}
 
   signOut() {
     this.authService.signOut().then(() => {
       // Esperar pela resolução da promisse para navegar para a página de login
+      this.popoverController.dismiss();
       this.router.navigateByUrl('/', { replaceUrl: true });
     });
   }
